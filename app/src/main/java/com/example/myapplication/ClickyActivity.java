270b1102f2a845f01248d35ce971b448546e02f0
Zhang Rick <zhang.rick@northeastern.edu>
2024-02-10 10:00:20 +0000

    Clicky Clicky On pressed functionality done

--- app/src/main/java/com/example/myapplication/ClickyActivity.java
+++ app/src/main/java/com/example/myapplication/ClickyActivity.java
@@ -11,12 +11,54 @@ import android.widget.Toast;
 public class ClickyActivity extends AppCompatActivity {
 
 
+private Button aBtn, bBtn, cBtn, dBtn, eBtn, fBtn, aboutMeBtn;
 
     private TextView pressedTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky);
+
+pressedTv = findViewById(R.id.main_tv_pressed);
+
+
+aBtn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        pressedTv.setText("Pressed: A");
+    }
+});
+bBtn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        pressedTv.setText("Pressed: B");
+    }
+});
+cBtn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        pressedTv.setText("Pressed: C");
+    }
+});
+dBtn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        pressedTv.setText("Pressed: D");
+    }
+});
+eBtn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        pressedTv.setText("Pressed: E");
+    }
+});
+fBtn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        pressedTv.setText("Pressed: F");
+    }
+});
+
 
 
     }
